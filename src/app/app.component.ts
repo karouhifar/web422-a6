99151860a06bbd8d65ddaa9ad375e0adbdc62e3e
Kamyab <karouhifar@gmail.com>
2021-04-10 02:24:44 -0400

    Developed a new feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 /*********************************************************************************
- * WEB422 – Assignment 05
+ * WEB422 – Assignment 06
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
  * assignment has been copied manually or electronically from any other source (including web sites) or
  * distributed to other students.
  *
  * Name: Kamyab Rouhifar Student ID: 147742183 Date: 2021-03-26
+ * 
+ * Online Link: https://kamyab-spotify.vercel.app/
  *
  ********************************************************************************/
 
